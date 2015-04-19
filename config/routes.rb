80ad0f851f6616ba83a655add42ebc35ed183c35
HiepDinh <hoanghiepitvnn@gmail.com>
2015-04-19 12:29:36 +0700

    update header, top_bar, menu, change font

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  resources :home, only: [:index], path: '/'
+  resources :products, only: [:index, :show]
   root 'home#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
